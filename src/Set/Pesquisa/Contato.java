5ab3a43bee02f252841e5800dfbe32afbf5cb470
kayo <kayohramos@gmail.com>
2023-09-27 15:01:36 -0300

    feat: set-pesquisa, set-ordenacao

---
+++ src/Set/Pesquisa/Contato.java
@@ -0,0 +1,41 @@
+import java.util.Objects;
+
+public class Contato {
+  //atributos
+  private String nome;
+  private int numero;
+
+  public Contato(String nome, int numero) {
+    this.nome = nome;
+    this.numero = numero;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+    public boolean equals(Object o) {
+     if (this == o) return true;
+        if (!(o instanceof Contato)) return false;
+
+        Contato contato = (Contato) o;
+
+        return Objects.equals(getNome(), contato.getNome());
+    }
+
+  public int hashCode() {
+    return Objects.hash(getNome());
+  }
+
+  public String toString() {
+    return "{" + nome + "," + numero + "}";
+  }
+}
